a3b44f0eafd0fed4a5d88f108430b5f76864dca0
emmikoivisto <emetsi@gmail.com>
2020-12-01 10:25:45 +0000

    deparment class added

--- src/main/java/com/codeclan/example/demo/models/Department.java
+++ src/main/java/com/codeclan/example/demo/models/Department.java
@@ -1,4 +1,32 @@
 package com.codeclan.example.demo.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="departments")
+
 public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="name")
+    private String name;
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Department(){
+
+    }
+
 }
--- src/main/java/com/codeclan/example/demo/models/Employee.java
+++ src/main/java/com/codeclan/example/demo/models/Employee.java
@@ -12,7 +12,7 @@ public class Employee {
 
     @Column(name="first_name")
     private String firstName;
-    @Column(name="lasst_name")
+    @Column(name="last_name")
     private String lastName;
     @Column(name="employee_number")
     private int employeeNumber;
