efcadc22f66b7e24067a8ff17075432e6c5cdda9
emmikoivisto <emetsi@gmail.com>
2020-12-01 10:17:40 +0000

    initial commit

---
+++ src/main/java/com/codeclan/example/demo/models/Employee.java
@@ -0,0 +1,37 @@
+package com.codeclan.example.demo.models;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private int employeeNumber;
+
+    public Employee(String firstName, String lastName, int employeeNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employeeNumber = employeeNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(int employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+}
